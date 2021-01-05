ce424ba35bf5b3f11d6158707b7660434fcdedd6
Steven Monaghan <steven@pop-os.localdomain>
2021-01-05 16:21:32 -0500

    Add executable runner to desktop menu

--- install.sh
+++ install.sh
@@ -15,4 +15,25 @@ flask db init
 flask db migrate
 flask db upgrade
 
+# Install firefox
+sudo apt install firefox
+
+# Move desktop environment to proper folder
+currentPath=`pwd`
+desktopDir=~/.local/share/applications/archeo-strength.desktop
+execPath="$currentPath/ArcheoStrength"
+iconPath=$currentPath/resources/as_logo_short.png
+
+rm $desktopDir
+echo $currentPath 
+echo $execPath
+echo $iconPath
+
+touch $desktopDir
+echo "[Desktop Entry]" >> $desktopDir
+echo "Name=ArcheoStrength" >> $desktopDir
+echo "Exec=$execPath" >> $desktopDir
+echo "Terminal=false" >> $desktopDir
+echo "Type=Application" >> $desktopDir
+echo "Icon=$iconPath" >> $desktopDir
 
